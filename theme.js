f4f49f3ae42c1ece44a0154e00527c325d675c2b
evannuelsoeryadi <evannuel.soeryadi@gmail.com>
2026-06-14 14:54:17 +0700

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,54 @@
 
+// =====================
+// DEV THEME
+// =====================
+
+if(
+
+localStorage.getItem(
+"developerLogged"
+) === "true"
+
+){
+
+let theme =
+
+localStorage.getItem(
+"devTheme"
+);
+
+if(theme){
+
+document.body.classList.add(
+theme
+);
+
+}
+
+}
+
+// =====================
+// CHANGE THEME
+// =====================
+
+function setTheme(theme){
+
+localStorage.setItem(
+
+"devTheme",
+
+theme
+
+);
+
+document.body.className = "";
+
+document.body.classList.add(
+theme
+);
+
+}
+
+// expose global
+window.setTheme = setTheme;
+
